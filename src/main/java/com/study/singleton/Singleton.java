e868adbbe65d6fc8839fb64e1b0a1e10bead6a80
linhh99 <562317331@qq.com>
2019-09-14 22:35:14 +0800

    增加单例模式

---
+++ src/main/java/com/study/singleton/Singleton.java
@@ -0,0 +1,19 @@
+package com.study.singleton;
+
+/**
+ * 单例模式
+ */
+public class Singleton {
+
+    private static Singleton singleton;
+
+    private Singleton(){
+    }
+
+    public static Singleton getInstance(){
+        if (singleton == null){
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+}
